952e4c02e18fca87cd2c5aa14fdfbd47631d84d5
FranciscoLizarazu <pei.lizarazu@gmail.com>
2023-12-06 16:18:32 -0300

    modelos y cambios en el admin

--- project/cliente/models.py
+++ project/cliente/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Pais(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
+    def __str__(self):
+          return self.nombre
 
 
 class Cliente(models.Model):
@@ -10,4 +12,7 @@ class Cliente(models.Model):
         apellido = models.CharField(max_length=100)
         nacimiento = models.DateField(null=True, blank=True)
         pais_origen = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True, blank=True )
+        
+        def __str__(self):
+            return f"{self.nombre}{self.apellido}"
  
